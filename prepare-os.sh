01b2ac3405f017301eb134e6b4d1e34055b4bc11
Enam Mijbah Noor <enam.noor@reef.pl>
2024-02-13 18:59:03 +0600

    fix prepare os script

--- prepare-os.sh
+++ prepare-os.sh
@@ -5,9 +5,16 @@ DOCKER_BIN="$(command -v docker || true)"
 DOCKER_COMPOSE_BIN="$(command -v docker-compose || true)"
 JQ_BIN="$(command -v jq || true)"
 
-if [ -x "${DOCKER_BIN}" ] && [ -x "${DOCKER_COMPOSE_BIN}" ] && [ -x "${SENTRY_CLI}" ] && [ -x "${B2_CLI}" ] && [ -x "${AWS_CLI}" ] && [ -x "${JQ_BIN}" ]; then
-    echo "\e[31mEverything required is already installed!\e[0m";
-    exit 0;
+
+if [ ! -x "${DOCKER_BIN}" ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-get -y install docker-ce
+    usermod -aG docker "$USER"
+fi
+
+if [ ! -x "${DOCKER_COMPOSE_BIN}" ]; then
+    apt-get -y install docker-ce docker-compose
 fi
 
 if [ ! -x "${JQ_BIN}" ]; then
